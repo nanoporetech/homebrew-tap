98fedb7ac63005c70571cbcbfef2195baa773a6a
Roger Pettett <rmp@psyphi.net>
2019-07-25 17:48:26 +0100

    oops. numbering system was updated slightly

--- Casks/epi2me-agent.rb
+++ Casks/epi2me-agent.rb
@@ -1,5 +1,5 @@
 cask 'epi2me-agent' do
-  version '2019.7.9.2549693'
+  version '2019.7.9-2549693'
   sha256 'c5eb303220de9ed7881df69e0111c5b13d5d479c968ed7dcc6dcee03f369a3eb'
 
   url "https://cdn.oxfordnanoportal.com/software/metrichor-agent/epi2me-agent-#{version}.dmg"
