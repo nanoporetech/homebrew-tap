30d3db44c7c4817ee09f1a1653c30edba67ca36b
Roger Pettett <rmp@psyphi.net>
2021-02-11 17:08:07 +0000

    updated template for agent3

--- Casks/epi2me-agent.rb
+++ Casks/epi2me-agent.rb
@@ -2,7 +2,7 @@ cask 'epi2me-agent' do
   version '3.2.0-4704840'
   sha256 '6aefdb565e8a127dd7d4fec669ab3a39554c2908d9a365799385fde04ebe249f'
 
-  url "https://cdn.oxfordnanoportal.com/software/metrichor-agent/epi2me-agent-#{version}.dmg"
+  url "https://cdn.oxfordnanoportal.com/software/metrichor-agent/epi2me-agent3-#{version}.dmg"
   name 'EPI2ME Agent (epi2me-agent)'
   homepage 'https://metrichor.com/'
 
--- Casks/epi2me-agent@development.rb
+++ Casks/epi2me-agent@development.rb
@@ -2,7 +2,7 @@ cask 'epi2me-agent@development' do
   version 'RC5-4633181'
   sha256 'bee0c4aa59901f050cd48a9264947c9581cfa896dc6b07c612d2a7bae324c6ed'
 
-  url "https://cdn.oxfordnanoportal.com/software/metrichor-agent/development/epi2me-agent-#{version}.dmg"
+  url "https://cdn.oxfordnanoportal.com/software/metrichor-agent/development/epi2me-agent3-#{version}.dmg"
   name 'EPI2ME Agent (epi2me-agent@development)'
   homepage 'https://metrichor.com/'
 
