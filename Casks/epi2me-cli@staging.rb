9cdcfe6d541f8f40e05374d7a7e09611de3c5ef0
Gitlab Runner homebrew-tap-schedule <gitlab@>
2021-09-13 11:01:37 +0000

    automated update 2021-09-13T11:01:37 5906983

--- Casks/epi2me-cli@staging.rb
+++ Casks/epi2me-cli@staging.rb
@@ -1,6 +1,6 @@
 cask "epi2me-cli@staging" do
-  version "2021.8.10-5741359"
-  sha256 "b531a08f7fc5ff785c949cdb366fbe70b923bf8a53d9397613bb02443a945683"
+  version "2021.9.13-5905754"
+  sha256 "5175afcfbde7a7fa50076df28c6d046ebc5c9ceea18c40d77cbf760a0987664c"
 
   url "https://cdn.oxfordnanoportal.com/software/metrichor-agent/staging/epi2me-cli-macos-#{version}.zip"
   name "EPI2ME CLI (epi2me-cli@staging)"
