5dca9cf5c10af8402bfcf496d1d825b9b6ebe457
Roger Pettett <rmp@psyphi.net>
2021-12-21 12:26:20 +0000

    account for zip structural differences

--- Casks/epi2me-cli3@development.rb
+++ Casks/epi2me-cli3@development.rb
@@ -10,7 +10,7 @@ cask "epi2me-cli3@development" do
   postflight do
     FileUtils.chmod "ugo+x", "#{HOMEBREW_PREFIX}/Caskroom/epi2me-cli3@development/#{version}/epi2me-cli3-macos-#{version}"
     File.unlink "#{HOMEBREW_PREFIX}/bin/epi2me-cli3@development" if File.exist?("#{HOMEBREW_PREFIX}/bin/epi2me-cli3@development")
-    File.symlink("#{HOMEBREW_PREFIX}/Caskroom/epi2me-cli3@development/#{version}/epi2me-cli3-macos-#{version}", "#{HOMEBREW_PREFIX}/bin/epi2me-cli3@development")
+    File.symlink("#{HOMEBREW_PREFIX}/Caskroom/epi2me-cli3@development/#{version}/epi2me-cli3-macos-#{version}/epi2me-#{version}", "#{HOMEBREW_PREFIX}/bin/epi2me-cli3@development")
   end
 
   uninstall delete: "#{HOMEBREW_PREFIX}/bin/epi2me-cli3@development"
