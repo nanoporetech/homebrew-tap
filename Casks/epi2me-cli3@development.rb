fff3502f5abcc35bdaaa5135411b63d83bd99d66
Gitlab Runner homebrew-tap-schedule <gitlab@>
2022-01-31 13:53:13 +0000

    automated update 2022-01-31T13:53:13 6801029

--- Casks/epi2me-cli3@development.rb
+++ Casks/epi2me-cli3@development.rb
@@ -1,6 +1,6 @@
 cask "epi2me-cli3@development" do
-  version "3.0.0-6686866"
-  sha256 "4779d1867837d4cfb78e3c94e1d11bd08d8261bde4f4775641c8d5646e75b655"
+  version "3.0.0-rc.1-6800635"
+  sha256 "4cb3f71d282e3efbace27bef90ca9c6fd9bd047c13a8b15b14c7ebdf205ff3b9"
 
   url "https://cdn.oxfordnanoportal.com/software/metrichor-agent/development/epi2me-cli3-macos-#{version}.zip"
   name "EPI2ME CLI3 (epi2me-cli3@development)"
