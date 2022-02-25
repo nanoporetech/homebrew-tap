3babbbe24a89285a54b5bc5716e1390dbf17daef
Gitlab Runner homebrew-tap-schedule <gitlab@>
2022-02-25 15:00:29 +0000

    automated update 2022-02-25T15:00:29 7078935

--- Casks/epi2me-labslauncher.rb
+++ Casks/epi2me-labslauncher.rb
@@ -1,11 +1,10 @@
 cask "epi2me-labslauncher" do
-  version "v2.0.1"
-  sha256 "030e2b82f0cf659171eaaec1f80ba01eb3f74c3fb43831a2e35fcb479ef7db9a"
+  version ""
+  sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
 
-  url "https://github.com/epi2me-labs/labslauncher/releases/download/v2.0.1/ont-epi2melabs-launcher-v2.0.1-macos.dmg"
+  url ""
   name "EPI2ME Labs Launcher (epi2me-labslauncher)"
-
   homepage "https://labs.epi2me.io"
 
-  uninstall delete: "#{HOMEBREW_PREFIX}/bin/epi2me-labslauncher"
+  app 'EPI2ME Labs Launcher'
 end
