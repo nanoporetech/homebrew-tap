f74cc3f70fff2037c4a1bae8dcad4ccbe8eba90f
Gitlab Runner homebrew-tap-schedule <gitlab@>
2023-02-09 17:37:31 +0000

    automated update 2023-02-09T17:37:31 10024441

--- Casks/epi2me-agent@development.rb
+++ Casks/epi2me-agent@development.rb
@@ -1,6 +1,6 @@
 cask 'epi2me-agent@development' do
-  version '3.6.0-9894888'
-  sha256 '2ae9573af31c3af06beb9fd1640f0e3da3daffec745aee5303d156f3edba7fc0'
+  version '3.6.1-10024112'
+  sha256 'dafd5c1c6888a3f081f764ab770ed2842368cce819c732348b8b2d435964ad3e'
 
   url "https://cdn.oxfordnanoportal.com/software/metrichor-agent/development/epi2me-agent3-#{version}.dmg"
   name 'EPI2ME Agent (epi2me-agent@development)'
