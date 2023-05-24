5e3f40911a082bb30e33dcbfc8ea8c1aa2b104ed
Gitlab Runner homebrew-tap-schedule <gitlab@>
2023-05-24 10:41:50 +0000

    automated update 2023-05-24T10:41:50 11106547

--- Casks/epi2me-labslauncher.rb
+++ Casks/epi2me-labslauncher.rb
@@ -1,10 +1,10 @@
 cask "epi2me-labslauncher" do
-  version "4.1.3"
-  sha256 "32a81199bcb9f252b08f9e300cdeb1ff14e0791e81295bfe73aeffe2b9edcf69"
+  version "5.0.0"
+  sha256 ""
 
-  url "https://ont-exd-int-s3-euwst1-epi2me-labs.s3.amazonaws.com/installers/EPI2MELabs-4.1.3.pkg"
+  url "https://ont-exd-int-s3-euwst1-epi2me-labs.s3.amazonaws.com/installers/EPI2MELabs-5.0.0.pkg"
   name "EPI2ME Labs Launcher (epi2me-labslauncher)"
   homepage "https://labs.epi2me.io"
 
-  pkg "EPI2MELabs-4.1.3.pkg", allow_untrusted: true
+  pkg "EPI2MELabs-5.0.0.pkg", allow_untrusted: true
 end
