87a0bab593f782ac0e1bcdec13848e5b5410c515
Gitlab Runner homebrew-tap-schedule <gitlab@>
2023-06-07 09:51:33 +0000

    automated update 2023-06-07T09:51:33 11332547

--- Casks/dorado.rb
+++ Casks/dorado.rb
@@ -1,6 +1,6 @@
 cask "dorado" do
-  version ""
-  sha256 ""
+  version "0.3.0"
+  sha256 "b348331a9a9b637818c96ab032ea148adf7945b73ab21977f5f94e3ea9fff331"
 
   url "https://cdn.oxfordnanoportal.com/software/analysis/dorado-#{version}-osx-arm64.tar.gz"
   name "Dorado libtorch Basecaller for Oxford Nanopore reads"
