918da049d73ae58579f68afd467dedf79f0cdab0
Gitlab Runner homebrew-tap-schedule <gitlab@>
2024-01-30 14:41:51 +0000

    automated update 2024-01-30T14:41:51 14044933

--- Casks/minknow-ui.rb
+++ Casks/minknow-ui.rb
@@ -1,6 +1,6 @@
 cask "minknow-ui" do
-  version "5.8.13"
-  sha256 "b3576021a3fa3128b94e0f77ed461946ba19accc3cfd546828166ab80e9c7be5"
+  version ""
+  sha256 ""
 
   url "https://cdn.oxfordnanoportal.com/software/MinKNOW/MinKNOW-UI-OSX-#{version}-arm.dmg"
   name "MinKNOW UI for Oxford Nanopore Sequencing"
