abfb5ea05508c067bbd8be91b94c3343c8c574bb
Gitlab Runner homebrew-tap-schedule <gitlab@>
2024-09-03 17:22:14 +0000

    automated update 2024-09-03T17:22:14

--- Casks/epi2me-one-x86_64.rb
+++ Casks/epi2me-one-x86_64.rb
@@ -1,12 +1,12 @@
 cask "epi2me-one-x86_64" do
-  version ""
-  sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+  version "5.1.14-x64"
+  sha256 "21481ee8da146c8abb3d744fb23891370317de729726f61a4f02e0dc102ed779"
 
-  url ""
+  url "https://ont-exd-int-s3-euwst1-epi2me-labs.s3.amazonaws.com/installers/EPI2ME-5.1.14-x64.pkg"
   name "EPI2ME ONE (epi2me-one-x86_64)"
   homepage "https://labs.epi2me.io"
 
-  pkg "EPI2ME-.pkg", allow_untrusted: true
+  pkg "EPI2ME-5.1.14-x64.pkg", allow_untrusted: true
   conflicts_with cask: "epi2me-one-arm64"
   depends_on arch: :x86_64
 end
