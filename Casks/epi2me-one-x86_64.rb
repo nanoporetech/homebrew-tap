1ecc724d68b6b5d706055626f82439077a4048cf
Gitlab Runner homebrew-tap-schedule <gitlab@>
2024-10-04 17:51:13 +0000

    automated update 2024-10-04T17:51:13

--- Casks/epi2me-one-x86_64.rb
+++ Casks/epi2me-one-x86_64.rb
@@ -1,12 +1,12 @@
 cask "epi2me-one-x86_64" do
-  version "5.2.0-x64"
-  sha256 "91df78034e52c26b0a64dab71f48d927446e0663bc91215a4d9a47d08a042960"
+  version "5.2.1-x64"
+  sha256 "4f982db73cab6c83610f32b70731d2151b468098d3e22074b55bccdc98a5ad02"
 
-  url "https://ont-exd-int-s3-euwst1-epi2me-labs.s3.amazonaws.com/installers/EPI2ME-5.2.0-x64.pkg"
+  url "https://ont-exd-int-s3-euwst1-epi2me-labs.s3.amazonaws.com/installers/EPI2ME-5.2.1-x64.pkg"
   name "EPI2ME ONE (epi2me-one-x86_64)"
   homepage "https://labs.epi2me.io"
 
-  pkg "EPI2ME-5.2.0-x64.pkg", allow_untrusted: true
+  pkg "EPI2ME-5.2.1-x64.pkg", allow_untrusted: true
   conflicts_with cask: "epi2me-one-arm64"
   depends_on arch: :x86_64
 end
--- Casks/epi2me-one.rb
+++ Casks/epi2me-one.rb
@@ -1,12 +1,12 @@
 cask "epi2me-one" do
-  version "5.2.0-arm64"
-  sha256 "80563dd7e98ce37b299d7ef50c92acb5ff2a22fd61b4908ee473f75372e050c3"
+  version "5.2.1-arm64"
+  sha256 "2b7699746ec05ea8dcb98c824b24ed6d392db90599a4a88a70e1dfbe0683b90a"
 
-  url "https://ont-exd-int-s3-euwst1-epi2me-labs.s3.amazonaws.com/installers/EPI2ME-5.2.0-arm64.pkg"
+  url "https://ont-exd-int-s3-euwst1-epi2me-labs.s3.amazonaws.com/installers/EPI2ME-5.2.1-arm64.pkg"
   name "EPI2ME ONE (epi2me-one)"
   homepage "https://labs.epi2me.io"
 
-  pkg "EPI2ME-5.2.0-arm64.pkg", allow_untrusted: true
+  pkg "EPI2ME-5.2.1-arm64.pkg", allow_untrusted: true
   conflicts_with cask: "epi2me-one-x86_64"
   depends_on arch: :arm64
 end
